b603019fc1044b6de38e3324680a90c49151d560
Shawn <smilligan@gmail.com>
2015-10-14 17:50:02 -0400

    bluetooth_runner conflicts...

--- Hangar/bluetooth_runner.py
+++ Hangar/bluetooth_runner.py
@@ -6,8 +6,11 @@ import time
 
 my_class = BluetoothManager.BluetoothManager()
 my_class.bluetooth_start()
+print "ready for device"
 phone = my_class.add_device()
-my_class.send_data(phone, "hello")
-while(True):
-    print my_class.receive_data()
+nearby_devices = my_class.discover_devices()
+for btmac, name in nearby_devices.iteritems():
+    print "BTMac: ", btmac, "| Name: ", name
+
+my_class.connect_device(nearby_devices.get("30:15:01:13:10:96"))
 my_class.bluetooth_stop()
