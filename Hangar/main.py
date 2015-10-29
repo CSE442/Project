e9d5ed204ee3cfb9f8b9fb0f6bfb93e7fcf7f1cf
Shawn <smilligan@gmail.com>
2015-10-28 20:24:33 -0400

    update on main.py and main_bluetooth.py to help with disambiguity of in and out channels

--- Hangar/main.py
+++ Hangar/main.py
@@ -11,13 +11,13 @@ from   channel           import *
 from   main_unity        import *
 from   main_bluetooth    import *
 from   message_generator import MessageGenerator
-from   BluetoothManager  import BluetoothManager
+from   bluetooth_manager  import BluetoothManager
 
 def main():
     # Create the communication channels between threads
-    bluetooth_in_channel,       main_bluetooth_out_channel = Channel()
-    main_bluetooth_in_channel,  bluetooth_out_channel      = Channel()
-    main_unity_in_channel,      unity_out_channel          = Channel()
+    bluetooth_send_channel,       main_bluetooth_receive_channel = Channel()
+    main_bluetooth_send_channel,  bluetooth_receive_channel      = Channel()
+    main_unity_send_channel,      unity_receive_channel          = Channel()
 
     # Create the bluetooth manager class
     bluetooth_manager = BluetoothManager()
@@ -26,26 +26,26 @@ def main():
     main_thread_id          = thread.get_ident()
 
     # Spawn the Bluetooth Transmitter Thread
-    bluetooth_in_thread_id  = thread.start_new_thread(main_bluetooth_in,
-                                                      (bluetooth_in_channel,
+    bluetooth_send_thread_id  = thread.start_new_thread(main_bluetooth_send,
+                                                      (bluetooth_send_channel,
                                                           bluetooth_manager,))
 
     # Spawn the Bluetooth Receiver Thread
-    bluetooth_out_thread_id = thread.start_new_thread(main_bluetooth_out,
-                                                      (bluetooth_out_channel,
+    bluetooth_receive_thread_id = thread.start_new_thread(main_bluetooth_receive,
+                                                      (bluetooth_receive_channel,
                                                           bluetooth_manager,))
 
     # Spawn the Visualizer Thread
     unity_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     unity_socket.bind((VISUALIZER_ADDRESS, VISUALIZER_PORT))
     unity_socket.listen(1)
-    unity_out_thread_id = thread.start_new_thread(main_unity,
+    unity_receive_thread_id = thread.start_new_thread(main_unity,
                                                   ( unity_socket
-                                                  , unity_out_channel
+                                                  , unity_receive_channel
                                                   ))
 
     # Receive bluetooth messages
-    for message in MessageGenerator(main_bluetooth_out_channel):
+    for message in MessageGenerator(main_bluetooth_receive_channel):
         # this will loop forever with the next message from bluetooth
         print message
 
